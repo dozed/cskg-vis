f46d8cdd22e07598c223fcc0d48d226ab32a0018
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2023-04-21 10:30:19 +0200

    Added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import request from "superagent";
+
+import logo from './logo.svg';
+import './App.css';
+
+request.get("http://localhost:8000/api").then((res) => console.log(res));
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
