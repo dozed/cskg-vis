8f6be8db8a57b01847bc91cde0ce3a68e92ad2e7
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2023-04-21 12:19:37 +0200

    Show graph

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import request from "superagent";
 import {useEffect, useState} from "react";
+import Graph from "graphology";
+import {SigmaContainer, useLoadGraph} from "@react-sigma/core";
+import {useWorkerLayoutForceAtlas2} from "@react-sigma/layout-forceatlas2";
+import {useLayoutCircular} from "@react-sigma/layout-circular";
+import "@react-sigma/core/lib/react-sigma.min.css";
 
 import './App.css';
 
@@ -8,24 +13,67 @@ const exampleDois = [
   'https://doi.org/10.18653/v1/2020.coling-main.448'
 ];
 
-function getPartialDoi(doi) {
+const getPartialDoi = (doi) => {
   return doi.replace('https://doi.org/', '');
 }
 
-function App() {
-  const [doi, setDoi] = useState(null);
+const ForceAtlas2Layout = () => {
+  const { start, kill, isRunning } = useWorkerLayoutForceAtlas2({ settings: { slowDown: 10 } });
+
+  useEffect(() => {
+    // start FA2
+    start();
+    return () => {
+      // Kill FA2 on unmount
+      kill();
+    };
+  }, [start, kill]);
+
+  return null;
+};
+
+const App = () => {
+  const [doi, setDoi] = useState('');
+  const [graph, setGraph] = useState(new Graph());
+
+  const StatementsGraph = ({ graph }) => {
+    const { positions, assign } = useLayoutCircular();
+    const loadGraph = useLoadGraph();
+
+    useEffect(() => {
+      loadGraph(graph);
+      assign();
+    }, [loadGraph, graph]);
+
+    return null;
+  };
 
   useEffect(() => {
     if (doi) {
       const partialDoi = getPartialDoi(doi);
       request.get(`/api/statements/by-doi/${partialDoi}`)
         .then((res) => {
-          console.log(res);
+          console.log(res.body.data);
+
+          const edges = res.body.data;
+          const nodes = new Set(edges.map(e => e.s).concat(edges.map(e => e.o)));
+
+          const graph = new Graph();
+
+          nodes.forEach(n => {
+            graph.addNode(n, { x: 0, y: 0, size: 15, label: n, color: "#FA4F40" });
+          });
+
+          edges.forEach((e, i) => {
+            graph.addEdgeWithKey(`${e.p}-${i}`, e.s, e.o, { label: e.p });
+          });
+
+          setGraph(graph);
         });
     }
   }, [doi]);
 
-  function fetchRandomDoi() {
+  const fetchRandomDoi = () => {
     request.get(`/api/dois/random`)
       .then((res) => {
         const doi = res.body.data;
@@ -38,7 +86,7 @@ function App() {
       <div className="App-header">
         <div>
           DOI:
-          <input type="text" value={doi} />
+          <input type="text" value={doi} onChange={(e) => setDoi(e.target.value)} />
           <input type="button" value="Show" />
           or
           <input type="button" value="Random" onClick={() => fetchRandomDoi()} />
@@ -55,7 +103,10 @@ function App() {
         </div>
       </div>
       <div className="App-content">
-
+        <SigmaContainer style={{ flex: 1, height: "auto", width: "auto" }}>
+          <StatementsGraph graph={graph} />
+          <ForceAtlas2Layout />
+        </SigmaContainer>
       </div>
     </div>
   );
